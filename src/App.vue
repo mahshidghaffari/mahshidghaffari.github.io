2c0c677ad8cac795092ea568497ccdbed74625b0
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2023-08-23 17:09:57 +0200

    clean style and add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,46 @@
 <template>
-    <transition name="text-transition">
-       <div>
-           <HeaderCo class="header-container"/>
-           <div class="app-container">
-               <div class="card-view">
-                   <router-view/>
-               </div>
-           </div>
-       </div>
-    </transition>
+  <div class=" app-container">
+      <shooting-star/>
+      <night-sky/>
+<!--      <div class="row">-->
+<!--          <div class="col-5 bg-dark">-->
+<!--              <router-view/>-->
+<!--          </div>-->
+
+<!--      </div>-->
+  </div>
+<!--    <div class="background-container">-->
+<!--        <shooting-star/>-->
+<!--&lt;!&ndash;<HeaderCo class="header-container"/>&ndash;&gt;-->
+<!--        <div class="app-container">-->
+<!--            <div class="card-view">-->
+<!--                <router-view/>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div>-->
+<!--            <b-button>Button</b-button>-->
+<!--        </div>-->
+<!--        <night-sky/>-->
+<!--&lt;!&ndash;        <transition name="mountains-transition">&ndash;&gt;-->
+<!--&lt;!&ndash;            <div class="mountains"></div>&ndash;&gt;-->
+<!--&lt;!&ndash;        </transition>&ndash;&gt;-->
+<!--    </div>-->
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
-import HeaderCo from "@/components/Header-co.vue";
+// import HeaderCo from "@/components/Header-co.vue";
+import shootingStar from "@/components/Shooting-star.vue";
+import nightSky from "@/components/night-sky.vue";
 import {useStore} from "vuex";
 import i18n from "@/composables/lang";
 
 export default defineComponent({
     components: {
-        HeaderCo,
+        // HeaderCo,
+        shootingStar,
+        nightSky
+
     },
     setup() {
         const store = useStore()
@@ -29,27 +50,19 @@ export default defineComponent({
 </script>
 
 <style>
-body {
-    margin: 0;
-    background: linear-gradient(90deg, #C7C5F4, #776BCC);
-}
-
 .app-container {
-    display: flex;
     justify-content: center;
-    height: 30vh;
+    height: 100vh;
 }
 
 .header-container {
     height: 10vh;
     text-align: center;
-
-
 }
 
 .card-view {
-    background: linear-gradient(90deg, #5D54A4, #7C78B8);
-    position: relative;
+    background: linear-gradient(90deg, #454353, #aeadc3);
+    position: absolute;
     width: 360px;
     box-shadow: 0px 0px 24px #5C5696;
     border-radius: 8px;
@@ -58,6 +71,57 @@ body {
     margin: 20px;
     max-width: 400px;
     width: 90%;
+    z-index: 6;
+    top: 35%;
+    right: 38%;
+    height: 30%;
+    opacity: 90%;
 }
+
+
+@keyframes move-background {
+    from {
+        -webkit-transform: translate3d(0px, 0px, 0px);
+    }
+    to {
+        -webkit-transform: translate3d(1000px, 0px, 0px);
+    }
+}
+@-webkit-keyframes move-background {
+    from {
+        -webkit-transform: translate3d(0px, 0px, 0px);
+    }
+    to {
+        -webkit-transform: translate3d(1000px, 0px, 0px);
+    }
+}
+
+@-moz-keyframes move-background {
+    from {
+        -webkit-transform: translate3d(0px, 0px, 0px);
+    }
+    to {
+        -webkit-transform: translate3d(1000px, 0px, 0px);
+    }
+}
+
+@-webkit-keyframes move-background {
+    from {
+        -webkit-transform: translate3d(0px, 0px, 0px);
+    }
+    to {
+        -webkit-transform: translate3d(1000px, 0px, 0px);
+    }
+}
+
+.background-container{
+    position: fixed;
+    top: 0;
+    left:0;
+    bottom: 0;
+    right: 0;
+}
+
+
 
 </style>
